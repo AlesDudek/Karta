9decec50acd85473a7b03905f1f6ee8503dce701
ales <mit.mit@seznam.cz>
2023-11-13 18:58:19 +0100

    add service and price

---
+++ js/welcome.js
@@ -0,0 +1,21 @@
+
+
+/*scroll šipka  */
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollUpArrow = document.getElementById("scrollUpArrow");
+
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 150) {
+        scrollUpArrow.style.display = "block";
+        } else {
+        scrollUpArrow.style.display = "none";
+        }
+    });
+
+    scrollUpArrow.addEventListener("click", function() {
+        window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+        });
+    });
+});
