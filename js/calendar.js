4974894ea5add1e37ecfc018d02dfe0e34e6e07e
ales <mit.mit@seznam.cz>
2023-12-02 18:40:32 +0100

    add respons

--- js/calendar.js
+++ js/calendar.js
@@ -6,8 +6,10 @@ const next = document.querySelector('.next');
 const todayBtn = document.querySelector('.today-btn');
 const gotoBtn = document.querySelector('.goto-btn');
 const dateInput = document.querySelector('.date-input');
-
-
+const eventDay = document.querySelector(".event-day");
+const eventDate = document.querySelector(".event-date");
+const eventsContainer = document.querySelector(".events");
+const addEventSubmit = document.querySelector(".add-event-btn");
 
 let today = new Date();
 let activeDay;
@@ -17,40 +19,55 @@ let year = today.getFullYear();
 const months = [
     "Leden","Únor","Březen","Duben","Květen","Červen","Červenec","Srpen","Září","Říjen","Listopad","Prosinec"
 ];
+let scrollEnabled = true;
+
+calendars.addEventListener('wheel', function(e) {
+    e.stopPropagation();
+});
+calendars.removeEventListener('wheel', function(e) {
+    e.stopPropagation();
+});
 
 //default events array
-const eventsArr = [
-    {
-        day: 16,
-        month: 11,
-        year: 2023,
-        events: [
-            {
-                title: "Event 1 lorem ipsum dolar sit genfa tersd dsad",
-                time: "10:00 AM",
-            },
-            {
-                title: "Event 2",
-                time: "11:00 AM",
-            },
-        ],
-    },
-    {
-        day: 18,
-        month: 11,
-        year: 2023,
-        events: [
-            {
-                title: "Event 1 lorem ipsum dolar sit genfa tersd dsad",
-                time: "10:00 AM",
-            },
-            {
-                title: "Event 2",
-                time: "11:00 AM",
-            },
-        ],
-    },
-];
+// const eventsArr = [
+//     {
+//         day: 16,
+//         month: 11,
+//         year: 2023,
+//         events: [
+//             {
+//                 title: "Event 1 lorem ipsum dolar sit genfa tersd dsad",
+//                 time: "10:00 AM",
+//             },
+//             {
+//                 title: "Event 2",
+//                 time: "11:00 AM",
+//             },
+//         ],
+//     },
+//     {
+//         day: 18,
+//         month: 11,
+//         year: 2023,
+//         events: [
+//             {
+//                 title: "Event 1 lorem ipsum dolar sit genfa tersd dsad",
+//                 time: "10:00 AM",
+//             },
+//             {
+//                 title: "Event 2",
+//                 time: "11:00 AM",
+//             },
+//         ],
+//     },
+// ];
+
+let eventsArr = [];
+
+//the call get
+getEvents();
+
+
 //fuction to add days
 
 function initCalendars(){
@@ -96,11 +113,15 @@ function initCalendars(){
             year === new Date().getFullYear() &&
             month === new Date().getMonth()
             ) {
+                activeDay = i;
+                getActiveDay(i);
+                updateEvents(i);
             // if event found also add event class
+            //add active on today at startup
                 if(event){
-                    days += `<div class = "day today event">${i}</div>`;
+                    days += `<div class = "day today active event">${i}</div>`;
                 } else{
-                    days += `<div class = "day today">${i}</div>`;
+                    days += `<div class = "day today active ">${i}</div>`;
                 }
         }
         //add remaing as it it
@@ -251,13 +272,17 @@ addEventTo.addEventListener('input', (e) =>{
     }
 });
 
-//lets create function to 
+//lets create function to
 function addListner(){
     const days = document.querySelectorAll('.day');
     days.forEach((day)=>{
         day.addEventListener("click", (e) =>{
             //set current day as active day
             activeDay = Number(e.target.innerHTML);
+
+            //call active day after click
+            getActiveDay(e.target.innerHTML);
+            updateEvents(Number(e.target.innerHTML));
 
             //remove active from already active day
             days.forEach((day) =>{
@@ -279,7 +304,195 @@ function addListner(){
                         }
                     });
                 }, 100);
+                //same with next month days
+            } else if(e.target.classList.contains("next-date")){
+                nextMonth();
+
+                setTimeout(() =>{
+                    //select all days off that month
+                    const days = document.querySelectorAll('.day');
+
+                    //after going to prev month aa ctive to clicked
+                    days.forEach((day) =>{
+                        if(!day.classList.contains("next-date") && day.innerHTML === e.target.innerHTML){
+                            day.classList.add("active");
+                        }
+                    });
+                }, 100);
+            } else {
+                //remaing current month days
+                e.target.classList.add("active");
             }
         });
     });
 }
+
+//lets show active day events and date at top
+//predelat na cesky kalendar
+
+function getActiveDay (date){
+    const day = new Date(year, month, date);
+    const dayName = day.toString().split(" ")[0];
+    eventDay.innerHTML = dayName;
+    eventDate.innerHTML = date + " " + months[month] + " " + year;
+}
+
+//function to show events of that day
+
+function updateEvents(date){
+    let events = " ";
+    eventsArr.forEach((event)=>{
+        //get events of active day only
+        if( date === event.day && month + 1 === event.month && year === event.year){
+            // then show event on document
+            event.events.forEach((event)=>{
+                events += `
+                <div class="event">
+                    <div class="title">
+                        <i class="fas fa-circle></i>
+                        <h3 class="event-title">${event.title}</h3>
+                    </div>
+                    <div class="event-time">
+                        <span class="event-time">${event.time}</span>
+                    </div>
+                </div>
+                `;
+            });
+        }
+    });
+    //if nothing found
+    if ((events === " ")){
+        events =
+        `<div class="no-event">
+        <h3>No Events</h3>
+        </div>`
+    }
+    eventsContainer.innerHTML = events;
+    // save events when update event called
+    saveEvents()
+}
+
+//lets create function to add events
+addEventSubmit.addEventListener("click", ()=>{
+    const eventTitle = addEventTitle.value;
+    const eventTimeFrom = addEventFrom.value;
+    const eventTimeTo = addEventTo.value;
+
+    // some validations
+    if(eventTitle === "" || eventTimeFrom === "" || eventTimeTo === ""){
+        alert("Please Fill all the fields")
+        return;
+    }
+    const timeFromArr = eventTimeFrom.split(":");
+    const timeToArr = eventTimeTo.split(":");
+
+    if (
+        timeFromArr.length !== 2 ||
+        timeToArr.length !== 2 ||
+        timeFromArr[0] > 23 ||
+        timeFromArr[1] > 59||
+        timeToArr[0] >23 ||
+        timeToArr[1] > 59){
+            alert("Invalid Time Fornat");
+    }
+    const timeFrom = convertTime(eventTimeFrom);
+    const timeTo = convertTime(eventTimeTo);
+
+    const newEvent = {
+        title: eventTitle,
+        time: timeFrom + " - " + timeTo,
+    };
+
+    let eventAdded = false;
+
+    //check if eventsarr not empty
+    if(eventsArr.length > 0){
+        //check if eventsarr not empty
+        eventsArr.forEach((item) =>{
+            if( item.day === activeDay && item.month === month + 1 && item.year === year){
+                item.events.push(newEvent);
+                eventAdded = true;
+            }
+        });
+    }
+
+    if(!eventAdded){
+        eventsArr.push({
+            day: activeDay,
+            month: month + 1,
+            year: year,
+            events: [newEvent]
+        });
+    }
+
+    //remove active from add event form
+    addEventContainer.classList.remove("active")
+    // clear the fields
+    addEventTitle.value = "";
+    addEventFrom.value = "";
+    addEventTo.value = "";
+
+    // show current added event
+    updateEvents(activeDay);
+
+    //also add event class to newly added day if not already
+    const activeDayElem = document.querySelector(".day.active");
+    if(!activeDayElem.classList.contains("event")){
+        activeDayElem.classList.add("event");
+    }
+
+});
+
+function convertTime(time){
+    let timeArr = time.split(":");
+    let timeHour = timeArr[0];
+    let timeMin = timeArr[1];
+    let timeFormat = timeHour >= 12 ? "PM":"AM";
+    timeHour = timeHour % 12 || 12;
+    time = timeHour + ":" + timeMin + " " + timeFormat;
+    return time;
+}
+
+//lets create a function to remove events on click
+eventsContainer.addEventListener("click", (e)=>{
+    
+    if(e.target.classList.contains("event")){
+        //tady je problem
+        const eventTitle = e.target.children[0].children[1].innerHTML;
+        console.log(eventTitle);
+        // get the title of event than search in array by title and delete
+        eventsArr.forEach((event)=>{
+            if(event.day === activeDay && event.month + 1 && event.year === year){
+                event.events.forEach((item, index) => {
+                    if(item.title === eventTitle){
+                        event.events.splice(index, 1);
+                    }
+                });
+                // if no event remaing on that dat remove complete day
+                if(event.events.length === 0){
+                    eventsArr.splice(eventsArr.indexOf(event), 1);
+                    // after remove complet day also remove active class of that day
+                    const activeDayElem = document.querySelector(".day.active");
+                    if (activeDayElem.classList.contains("event")){
+                        activeDayElem.classList.remove("event");
+                    }
+                }
+            }
+        });
+        //aftter removing form array update event
+        updateEvents(activeDay);
+    }
+});
+
+//lets store events in local storage get from there
+function saveEvents(){
+    localStorage.setItem("events", JSON.stringify(eventsArr));
+}
+
+function getEvents(){
+    if (localStorage.getItem("event" === null)){
+        return;
+    }
+    eventsArr.push( ...JSON.parse(localStorage.getItem("events")));
+}
+//44:36
